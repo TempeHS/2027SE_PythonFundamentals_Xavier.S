17d35c2d00ca4568575c13bd3afe894eb0fbef4d
xavierspurrier <xavier.spurrier1@education.nsw.gov.au>
2026-02-19 13:25:04 +0000

    upload completed interpreter.py

---
+++ 1-Conditionals/interpreter/interpreter.py
@@ -0,0 +1,17 @@
+x = input("What is Your number")
+y = input("What sign are you using")
+z = input("What is Your second number")
+
+
+x = int(x)
+z = int(z)
+
+if y == "+":
+    result = x + z
+elif y == "-":
+    result = x - z
+elif y == "*":
+    result = x * z
+elif y == "/":
+    result = x / z
+print(result)
