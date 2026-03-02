cc600e94fe6a1c2e4711973ecae2ae982a43d9d0
xavierspurrier <xavier.spurrier1@education.nsw.gov.au>
2026-03-02 22:40:23 +0000

    Upload completed plates.py

---
+++ plates/plates.py
@@ -0,0 +1,31 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid():
+    if(
+	count()
+	letter_check()
+    
+	): 
+	return True
+
+def count(s):
+    if len(s) >= 2 and len(s) <= 6:
+        return true 
+
+def numberletter()
+    if plate.isalnum():
+        return true 
+
+
+def letter_check(f)
+    if f[:2].isalpha()
+		return true 
+
+
+main()
