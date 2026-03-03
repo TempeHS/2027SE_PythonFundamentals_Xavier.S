21b1bdc7958bf45430b3e9a0e3d1736bd3fe72b6
xavierspurrier <xavier.spurrier1@education.nsw.gov.au>
2026-03-03 02:57:52 +0000

    Upload completed plates.py

--- plates/plates.py
+++ plates/plates.py
@@ -1,31 +1,78 @@
 def main():
-    plate = input("Plate: ")
-    if is_valid(plate):
-        print("Valid")
+
+    user_input = input("What would you like your number plate to be? ")
+
+    if (
+        len_check(user_input)
+        and letter_check(user_input)
+        and valid_value_check(user_input)
+        and number_check(user_input)
+    ):
+        print("Valid!")
+
     else:
-        print("Invalid")
+        main()
+
+    return
 
 
-def is_valid():
-    if(
-	count()
-	letter_check()
-    
-	): 
-	return True
+def len_check(string):
 
-def count(s):
-    if len(s) >= 2 and len(s) <= 6:
-        return true 
-
-def numberletter()
-    if plate.isalnum():
-        return true 
+    if len(string) > 6:
+        print("Error! Number plate is too long! ")
+        return False
+    elif len(string) < 2:
+        print("Error! Number plate is too short! ")
+        return False
+    else:
+        return True
 
 
-def letter_check(f)
-    if f[:2].isalpha()
-		return true 
+def letter_check(string):
+    string_check = 0
+    for i in range(2):
+        if string[i].isalpha():
+            string_check += 1
+        else:
+            print("Error! Number plate does not start with two alphabetical values! ")
+            return False
+
+    if string_check >= 2:
+        return True
+
+
+def valid_value_check(string):
+    value = len(string)
+    for i in range(len(string)):
+        if string[i].isnumeric or string[i].isascii():
+            value -= 1
+        else:
+            print("Error! Only numbers and letters are allowed!")
+            return False
+
+    if value <= 0:
+        return True
+
+
+def number_check(string):
+    for i in range(len(string)):
+        if string[i].isnumeric():
+
+            _, middle, right = string.partition(string[i])
+
+            if right.isnumeric() == False and right != "":
+                print("Error! Number plate has a number before a letter")
+
+                return False
+
+            elif middle == "0":
+                print("Error! First number can't be 0!")
+                return False
+
+            else:
+                return True
+        else:
+            return True
 
 
 main()
