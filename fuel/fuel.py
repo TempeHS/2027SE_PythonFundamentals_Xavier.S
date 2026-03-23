913c88ae8697c498700d0f748e01dcd977fb3de1
xavierspurrier <xavier.spurrier1@education.nsw.gov.au>
2026-03-23 11:26:34 +0000

    Upload completed taqueria.py

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,9 +1,31 @@
 def main():
     while True:
-        fraction = input("What is your fraction")
-        
-        try:   
+        fraction = input("Fraction: ")
+
+        try:
             x_text, y_text = fraction.split("/")
+            x = int(x_text)
+            y = int(y_text)
+
+            if x > y:
+                continue
+
+            percentage = (x / y) * 100
+            percentage = round(percentage)
+
+            if percentage == 0:
+                print("E")
+            elif percentage >= 100:
+                print("F")
+            else:
+                print(f"{percentage}%")
+
+            break
+
+        except ValueError:
+            pass
+        except ZeroDivisionError:
+            pass
 
 
 main()
